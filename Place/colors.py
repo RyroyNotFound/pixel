1b6ad50ca3da6f931ed94ad48058f4c98a4b52fe
Pierre Louis NAVAUD <pierrelouis@navaud.fr>
2021-03-26 14:49:25 +0100

    s

---
+++ Place/colors.py
@@ -0,0 +1,13 @@
+from enum import Enum
+class Colors(Enum):
+    0 = (255,255,255) #blanc
+    1 = (0,0,0) # noir
+    2 = (255, 0, 0), # Red
+    3 = (51, 204, 51), # Green
+    4 = (0, 153, 255), # Blue
+    5 = (255, 51, 204), # Pink
+    6 = (255, 153, 0), # Orange
+    7 = (255, 255, 0), # Yellow
+    8 = (153, 0, 204), # Purple
+    9 = (128, 128, 128), # Gray
+]
--- Place/main.py
+++ Place/main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame import display, event, key
 from pygame.constants import K_KP1, K_KP2
+from colors import Colors
 
 pygame.init()
 display.set_caption("Place")
@@ -9,15 +10,15 @@ screen = display.set_mode((900,900))
 c_souris = False
 color = 1
 cells = []
-def init_grille(size_grille, size_cell):
+size_grille = (30,30)
+size_cell = (30,30)
+
+
+def init_grille(size_grille):
     for x in range(size_grille[0]):
-
         rows = []
-
-        for y in range(size_grille[1]):
-                
+        for y in range(size_grille[1]):                
             rows.append(0)
-
         cells.append(rows)
 
 
@@ -34,7 +35,13 @@ while running :
                 c_souris = True
                 pos = pygame.mouse.get_pos()
 
+                for x in range(size_grille[0]):
+                    for y in range(size_grille[1]):
+                           draw.rect(self.screen, Color(cells(x,y)),((x*size_cell[0],y*size_cell[1]),(size_cell[0],size_cell[1])))
                 
+
+
+
                 #fonction remplir couleur sur la case
         else :
             c_souris = False
@@ -44,8 +51,8 @@ while running :
   
     if key.get_pressed()[K_KP2]:
         color = 1
-       
- 
+
+
     screen.fill((255,255,255))
 
 
