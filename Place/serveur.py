2a781204745dcf26ca3c78a80bb872e3f7a6e8dc
Sylvain Decombe <sylvain.decombe@viacesi.fr>
2021-03-26 15:04:21 +0100

    ajout couleurs

--- Place/serveur.py
+++ Place/serveur.py
@@ -1,6 +1,19 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+COLORS = [
+    (255, 255, 255), # White
+    (0, 0, 0), # Black
+    (255, 0, 0), # Red
+    (51, 204, 51), # Green
+    (0, 153, 255), # Blue
+    (255, 51, 204), # Pink
+    (255, 153, 0), # Orange
+    (255, 255, 0), # Yellow
+    (153, 0, 204), # Purple
+    (128, 128, 128), # Gray
+]
+
 tab = [] #Cette liste contiendra ma map en 2D
 for i in range():
     tab.append([0] * 6) #Ajoute 30 colonnes de 30 entiers(int) ayant pour valeurs 0
@@ -12,7 +25,7 @@ def dessin_case():
 @app.route('/full', methods=['GET'])
 def retourne_tableau():
         print(f"OK Get !")
-        return ''
+        return tab
 
 print(tab)
 app.run()
