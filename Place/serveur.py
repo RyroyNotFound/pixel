9a1c493846a8a6829b517222ca923d9b500ee911
Sylvain Decombe <sylvain.decombe@viacesi.fr>
2021-03-26 14:49:47 +0100

    ajout routes

--- Place/serveur.py
+++ Place/serveur.py
@@ -1,17 +1,18 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+tab = [] #Cette liste contiendra ma map en 2D
+for i in range():
+    tab.append([0] * 6) #Ajoute 30 colonnes de 30 entiers(int) ayant pour valeurs 0
 
-@app.route('/hello', methods=['GET', 'POST'])
-def hello_world():
-    if request.method == 'POST':
-        nom = request.json['name']
-        print(f"Envoi de la requete post pour \"{nom}\"")
-        return nom
-    else :
-        print(f"Envoi de la requete get pour \"Jim\"")
-        return 'Bonjour Jim ! (GET)'
+@app.route('/place', methods=['POST'])
+def dessin_case():
+        return ''
 
+@app.route('/full', methods=['GET'])
+def retourne_tableau():
+        print(f"OK Get !")
+        return ''
 
-
+print(tab)
 app.run()
